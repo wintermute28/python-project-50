dc397cbb8a59634fb4e817eb6443e9b54285a289
wintermute28 <a28c@yandex.ru>
2022-09-06 13:40:01 +0300

    CodeClimate check, lint

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,7 @@ import json
 def main():
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows a difference."
-        )
+    )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     parser.add_argument("-f", "--format", help="set format of output")

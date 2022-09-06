ad940d8a9aa64b43f70fad69a7108ed702565496
wintermute28 <a28c@yandex.ru>
2022-09-06 11:54:38 +0300

    clear code

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,18 +4,19 @@ import json
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows a difference."
+        )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     parser.add_argument("-f", "--format", help="set format of output")
     args = parser.parse_args()
 
-
     def generate_diff(file1, file2):
         result = "{\n"
         first_file = json.load(open(file1))
         second_file = json.load(open(file2))
-        keys = sorted(list({x for x in {**first_file ,**second_file}}))
+        keys = sorted(list({x for x in {**first_file, **second_file}}))
         for key in keys:
             if first_file.get(key) == second_file.get(key):
                 result += f"    {key}: {bool_convert(second_file[key])}\n"
@@ -32,15 +33,13 @@ def main():
         result += "}"
         return result
 
-
     def bool_convert(input):
-        if input == True:
+        if input is True:
             input = 'true'
-        if input == False:
+        if input is False:
             input = 'false'
         return str(input)
 
-   
     print(generate_diff(args.first_file, args.second_file))
 
 

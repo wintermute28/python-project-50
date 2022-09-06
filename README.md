9ece7ffd3e1be0227a38053ab2b1abdf8520a8cf
wintermute28 <a28c@yandex.ru>
2022-09-06 11:44:02 +0300

    add diff of 2 json files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/wintermute28/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/wintermute28/python-project-50/actions)
+[![asciicast](https://asciinema.org/a/519182.svg)](https://asciinema.org/a/519182)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,48 @@
 #!/usr/bin/env python3
 import argparse
+import json
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
     parser.add_argument("-f", "--format", help="set format of output")
-    
     args = parser.parse_args()
-    print(args.first_file, args.second_file, args.format)
 
 
-if __name__ == '__main__':
+    def generate_diff(file1, file2):
+        result = "{\n"
+        first_file = json.load(open(file1))
+        second_file = json.load(open(file2))
+        keys = sorted(list({x for x in {**first_file ,**second_file}}))
+        for key in keys:
+            if first_file.get(key) == second_file.get(key):
+                result += f"    {key}: {bool_convert(second_file[key])}\n"
+                continue
+            if first_file.get(key) is None:
+                result += f"  + {key}: {bool_convert(second_file[key])}\n"
+                continue
+            if second_file.get(key) is None:
+                result += f"  - {key}: {bool_convert(first_file[key])}\n"
+                continue
+            if first_file.get(key) != second_file.get(key):
+                result += f"  - {key}: {bool_convert(first_file[key])}\n"
+                result += f"  + {key}: {bool_convert(second_file[key])}\n"
+        result += "}"
+        return result
+
+
+    def bool_convert(input):
+        if input == True:
+            input = 'true'
+        if input == False:
+            input = 'false'
+        return str(input)
+
+   
+    print(generate_diff(args.first_file, args.second_file))
+
+
+if __name__ == "__main__":
     main()
